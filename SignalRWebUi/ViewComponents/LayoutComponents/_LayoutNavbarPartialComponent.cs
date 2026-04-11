d5cf5266fb07dd35a2ea60ca20dae0d58e48dfed
Yusuf Kaya <yusuf04704@gmail.com>
2026-04-11 16:12:01 +0300

    Düzzenlemeler yapıldı

---
+++ SignalRWebUi/ViewComponents/LayoutComponents/_LayoutNavbarPartialComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRWebUi.ViewComponents.LayoutComponent
+{
+    public class _LayoutNavbarPartialComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ SignalRWebUi/ViewComponents/LayoutComponents/_LayoutSidebarPartialComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRWebUi.ViewComponents.LayoutComponents
+{
+    public class _LayoutSidebarPartialComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
