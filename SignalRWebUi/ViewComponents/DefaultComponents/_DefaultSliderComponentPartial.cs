9fda070850854a0ed8e2c0348c655d5702415577
Yusuf Kaya <yusuf04704@gmail.com>
2026-04-28 23:24:57 +0300

    view sayfaları eklendi

---
+++ SignalRWebUi/ViewComponents/DefaultComponents/_DefaultSliderComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace SignalRWebUi.ViewComponents.DefaultComponents
+{
+    public class _DefaultSliderComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();  
+        }
+    }
+}
---
+++ SignalRWebUi/ViewComponents/UILayoutComponents/_UILayoutScriptComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRWebUi.ViewComponents.UILayoutComponents
+{
+    public class _UILayoutScriptComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
