40eaa1431d580623a71cbffee75c2f9b4513db27
Yusuf Kaya <yusuf04704@gmail.com>
2026-05-30 18:06:19 +0300

    Tema için özelleştirmeler yapıldı.

--- SignalRWebUi/wwwroot/js/chat.js
+++ SignalRWebUi/wwwroot/js/chat.js
@@ -1,4 +1,4 @@
-var connection = new signalR.HubConnectionBuilder().withUrl("https://localhost:7186/SignalRHub").build();
+﻿var connection = new signalR.HubConnectionBuilder().withUrl("https://localhost:7195/SignalRHub").build();
 document.getElementById("sendbutton").disabled = true;
 
 connection.on("ReceiveMessage", function (user, message) {
